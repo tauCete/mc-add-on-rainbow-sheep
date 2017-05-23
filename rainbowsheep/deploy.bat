69674b3e95c709a6b6a1df71d2bfba7e0a6f8b67
TauCete <pixxels@yahoo.com>
2017-05-23 06:53:15 -0700

    Create repo from VS

---
+++ rainbowsheep/deploy.bat
@@ -0,0 +1,3 @@
+﻿* copy files to test world
+
+copy .\behavior_packs\rainbowsheep\*.* C:\Users\Bleu_\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\minecraftWorlds\wizardry\behavior_packs\rainbowsheep\
